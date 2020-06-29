1b73fd884bdae8a7ae97670a843f78d1264bc405
pravinpoudel <prvnpoudel4@gmail.com>
2020-06-29 12:12:27 +0545

    submenu toggle - added event listener to submenu items and toogleItem function created

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
-
+const items = document.querySelectorAll(".item");
 toggle.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
@@ -8,7 +8,24 @@ function toggleMenu() {
     menu.classList.remove("active");
     toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
   } else {
-    menu.classList.add(".active");
+    menu.classList.add("active");
     toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove(".submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+for (let item of items) {
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+    item.addEventListener("touch", toggleItem, false);
   }
 }
